f8da17e45702980a55b340bec8f6fcdb829b2fab
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2022-10-17 15:00:29 +0800

    feat: form表單測試

---
+++ src/components/FormHanlder.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <input
+      v-model="email"
+      type="email"
+      data-mail="email"
+    >
+
+    <form
+      data-test="form"
+      @submit.prevent="submit"
+    >
+      <input
+        v-model="form.email"
+        data-test="email"
+        type="email"
+      >
+
+      <textarea
+        v-model="form.description"
+        data-test="description"
+      />
+
+      <select
+        v-model="form.city"
+        data-test="city"
+      >
+        <option value="taipei">
+          Taipei
+        </option>
+        <option value="tainan">
+          Tainan
+        </option>
+      </select>
+
+      <input
+        v-model="form.subscribe"
+        data-test="subscribe"
+        type="checkbox"
+      >
+
+      <input
+        v-model="form.interval"
+        data-test="interval.weekly"
+        type="radio"
+        value="weekly"
+      >
+      <input
+        v-model="form.interval"
+        data-test="interval.monthly"
+        type="radio"
+        value="monthly"
+      >
+
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+<script setup>
+import { reactive, ref } from 'vue'
+
+const emit = defineEmits(['submit'])
+const email = ref('')
+
+const form = reactive({
+  email: '',
+  description: '',
+  city: '',
+  subscribe: false,
+  interval: ''
+})
+
+const submit = () => {
+  emit('submit', form)
+}
+</script>
---
+++ src/components/__tests__/FormHandler.spec.js
@@ -0,0 +1,70 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import FormHandler from '../FormHanlder.vue'
+
+describe('FormHandler', () => {
+  it('value of input should be my@mail.com', async () => {
+    const wrapper = mount(FormHandler)
+    const input = wrapper.get('[data-mail="email"]')
+    await input.setValue('my@mail.com')
+    /**
+     * setValue(): 要更改表單元素的值可以使用 setValue() 方法，setValue 接受一個參數，可以是字串或布林值，並且回傳的是一個 Promise。
+     * DOMWrapper: 透過 get() 或是 find() 成功找到目標元素時都會回傳一個圍繞 Wrapper API 的 DOM 元素的瘦包裝器 (thin wrapper)，而它有一個代表著 HTMLElement 的屬性 element，又因為在上面的情況目標元素為 input tag 所以此時 element 真實的值其實為 HTMLInputElement。
+     */
+    expect(input.element.value).toBe('my@mail.com')
+  })
+
+  it('after set value, value of mail should be my@mail.com', async () => {
+    const wrapper = mount(FormHandler)
+    await wrapper.get('[data-mail="email"]').setValue('my@mail.com')
+    expect(wrapper.vm.email).toBe('my@mail.com')
+  })
+
+  it('fill up form', async () => {
+    const wrapper = mount(FormHandler)
+
+    const email = 'name@mail.com'
+    const description = 'Lorem ipsum dolor sit ames'
+    const city = 'taipei'
+    const subscribe = true
+
+    await wrapper.get('[data-test="email"]').setValue(email)
+    await wrapper.get('[data-test="description"]').setValue(description)
+    await wrapper.get('[data-test="city"]').setValue(city)
+    await wrapper.get('[data-test="subscribe"]').setValue()
+    await wrapper.get('[data-test="interval.weekly"]').setValue()
+
+    // 在呼叫 setValue 的對象為 OPTION、CHECKBOX 或 RADIO 時， 如果沒有傳參數給 setValue 則表示為 checked 。
+    expect(wrapper.vm.form).toEqual({
+      email,
+      description,
+      city,
+      subscribe,
+      interval: 'weekly'
+    })
+  })
+
+  it('submits the form', async () => {
+    const wrapper = mount(FormHandler)
+    const email = 'name@mail.com'
+    const description = 'Lorem ipsum dolor sit amet'
+    const city = 'taipei'
+    const subscribe = true
+
+    await wrapper.get('[data-test="email"]').setValue(email)
+    await wrapper.get('[data-test="description"]').setValue(description)
+    await wrapper.get('[data-test="city"]').setValue(city)
+    await wrapper.get('[data-test="subscribe"]').setValue(subscribe)
+    await wrapper.get('[data-test="interval.monthly"]').setValue()
+
+    await wrapper.get('[data-test="form"]').trigger('submit.prevent')
+
+    expect(wrapper.emitted('submit')[0][0]).toEqual({
+      email,
+      description,
+      city,
+      subscribe,
+      interval: 'monthly'
+    })
+  })
+})
